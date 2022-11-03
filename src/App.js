24784db7f3a38817449ba112812b556b9d0499df
“SuleRas” <sm.ras@hotmailco.uk>
2022-11-03 21:49:33 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HelloReact from "./components/HelloReact";
+
+// TODO: Add a comment explaining what this function is doing
+// This function is a functional component that helps us split the UI into distinct parts.
+// In this case, we are returning another component, <HelloReact/> from it.
+function App() {
+  return <h1>Hello! My name is Suleiman </h1>;
+}
+
+export default App;
+
+//hello react should point to this folder (here).
+
+//publish react test1
